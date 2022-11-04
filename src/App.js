4dfa67c81278a645833dbcb5fd0488e6468bd64f
tesfaye robelle <tdebella91@gail.com>
2022-11-04 02:13:35 -0400

    npx create-react-app and git repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eShop</a>{" "}
+      </header>
+      <main>list products</main>
+      <footer>All rights reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
